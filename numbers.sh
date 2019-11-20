00028936dc529d296e99ee5bc8ee03fc3552a2d7
Jose Arellano <jarellano@chapman.edu>
2019-11-19 17:00:09 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -4,18 +4,17 @@
 
 echo "Enter a positive Number: "
 read NUMBER
+
 N=1
 while [ $N -le "$NUMBER" ]
 do	
-
-	if $((NUMBER%2)) -eq 0
+	if [ $((N%2)) -eq 0 ]
 	then	
 		echo "Your number is even."
-		
 	else
 		echo "Your number is odd."
-	N=$((N+1))
 	fi
+	N=$((N+1))
 done	
 
 
